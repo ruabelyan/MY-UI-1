61c22a475c37a878da4325aeec330a1fd54da6ff
Armen Mesropyan <amesropyan@bangits.com>
2022-04-25 11:13:53 +0400

    feat(design-tokens): add defaultValues for colors, radius, shadows
    Signed-off-by: Armen Mesropyan <amesropyan@bangits.com>

--- packages/core/src/design-tokens/radius/defaultValues.ts
+++ packages/core/src/design-tokens/radius/defaultValues.ts
@@ -1,6 +1,11 @@
 import { RadiusThemeConfig } from './types';
 
 const radiusDefaultValues: RadiusThemeConfig = {
+  xs: '4px',
+  sm: '8px',
+  md: '12px',
+  lg: '16px',
+  xl: '20px',
   circle: '10000rem',
 };
 
--- packages/core/src/design-tokens/shadows/defaultValues.ts
+++ packages/core/src/design-tokens/shadows/defaultValues.ts
@@ -1,7 +1,11 @@
 import { ShadowThemeConfig } from './types';
 
 const shadowDefaultValues: ShadowThemeConfig = {
-  sm: '0 0 1px 0 rgb(8 11 14 / 6%), 0 16px 16px -1px rgb(8 11 14 / 10%)',
+  xsm: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+  sm: '0px 1px 2px -1px rgba(16, 24, 40, 0.1), 0px 1px 3px rgba(16, 24, 40, 0.1)',
+  md: '0px 2px 4px -2px rgba(16, 24, 40, 0.1), 0px 4px 6px -1px rgba(16, 24, 40, 0.1)',
+  lg: '0px 4px 6px -4px rgba(16, 24, 40, 0.1), 0px 10px 15px -3px rgba(16, 24, 40, 0.1)',
+  xlg: '0px 8px 10px -6px rgba(16, 24, 40, 0.1), 0px 20px 25px -5px rgba(16, 24, 40, 0.1)',
 };
 
 export default shadowDefaultValues;
